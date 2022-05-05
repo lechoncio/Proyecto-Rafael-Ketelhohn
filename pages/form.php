d8505bf55050a50ae342fb61a4c4801b525b6239
Rafael Ketelhohn <rafaelk@gmail.com>
2022-05-05 18:29:45 -0300

    prueba 1 de formulario funcionando

---
+++ pages/form.php
@@ -0,0 +1,21 @@
+<?php
+
+$edad = $_POST["gridRadios"];
+$nombre = $_POST["nombre"];
+$mail = $_POST["mail"];
+$comentarios = $_POST["comentarios"];
+$pdf = $_POST["pdf"];
+
+$mensaje = "Este mensaje fué enviado por:" . $nombre . ",\r\n";
+$mensaje .= "Su email es:" . $mail . "\r\n";
+$mensaje .= "Edad:" . $edad . "\r\n";
+$mensaje .= "Mensaje:" . $_POST["comentarios"] . "\r\n";
+$mensaje .= "Enviado el:" . date("d/m/y", time());
+
+$para = "rafaelk@gmail.com";
+$asunto = "Mail de Segway"
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+header("location:exito.html")
+
+?>
